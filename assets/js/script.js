3fcb40bf642ee72ddd6ef9bcafc9901ad8985902
Deanjer <220509@student.glu.nl>
2023-02-15 11:05:54 +0100

    m6_c5

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,12 @@
     Als de 2 autos tegen elkaar rijden volgt er een explosie
 */
 var crashCounter = 0; // stel de crashcounter bij begin van de game op 0 in
-
+var a = document.getElementById("crashCounter");
+a.innerHTMl = "test2";
+//document.getElementById("crashCounter").innerHTML = "I have changed!";
 for (let index = 0; index < 5; index++) {  // plaats 5 bomen random op het speelvak
     var img = document.createElement('img');
-    img.src = 'assets/omg/tree.jpg';  // URL van boom-afbeelding
+    img.src = 'assets/img/tree.jpg';  // URL van boom-afbeelding
     img.className = "tree";
     img.style.left = Math.random(80) * 80 + "%";
     img.style.top = Math.random(80) * 80 + "%";
@@ -24,7 +26,7 @@ car.style.top = 0;  // reset de auto aan het begin van de game
 car.style.left = 0;
 
 document.body.onkeydown = function () {  //toetsenbord pijltjes-toets events
-    var e = event.keyCode;
+    var e = event.keyCode; console.log(e)
     switch (e) {
         case 40: //down
             car.style.transform = 'rotate(90deg)';
@@ -35,9 +37,10 @@ document.body.onkeydown = function () {  //toetsenbord pijltjes-toets events
             break;
         case 37: //left
             car.style.transform = 'rotate(180deg)';
-            car.style.left = (parseInt(car.style.left)) + 5 + "px";
+            car.style.left = (parseInt(car.style.left)) - 5 + "px";
             if (crash() == true) {
-                car.style.left = (parseInt(car.style.left)) - 5 + "px";
+                console.log("hallo")
+                car.style.left = (parseInt(car.style.left)) + 5 + "px";
 
             }
             break;
@@ -49,11 +52,11 @@ document.body.onkeydown = function () {  //toetsenbord pijltjes-toets events
 
             }
             break;
-        case 37: //top
+        case 38: //top
             car.style.transform = 'rotate(-90deg)';
-            car.style.top = (parseInt(car.style.top)) + 5 + "px";
+            car.style.top = (parseInt(car.style.top)) - 5 + "px";
             if (crash() == true) {
-                car.style.top = (parseInt(car.style.top)) - 5 + "px";
+                car.style.top = (parseInt(car.style.top)) + 5 + "px";
 
             }
             break;
@@ -61,18 +64,20 @@ document.body.onkeydown = function () {  //toetsenbord pijltjes-toets events
     function crash() {
         trees = document.getElementsByClassName("tree"); // haal alle bomen op
         var overlap = false;
-        for (let index = 0; index < trees.length; index++) { // loop door ale bomen heen, en kijk of er raakvlak is met de car
+        for (let index = 0; index < trees.length; index++) { // loop door alle bomen heen, en kijk of er raakvlak is met de car
             overlap = !(car.getBoundingClientRect().right < trees[index].getBoundingClientRect().left ||
                 car.getBoundingClientRect().left > trees[index].getBoundingClientRect().right ||
                 car.getBoundingClientRect().bottom < trees[index].getBoundingClientRect().top ||
                 car.getBoundingClientRect().top > trees[index].getBoundingClientRect().bottom);
        if (overlap) { // er is een crash, tel de crashcounter op
                 crashCounter += 1;
+                document.getElementById("crashCounter").innerHTML = "<b>Crash counter:</b> " + crashCounter;
+                console.log(crashCounter)
                 return true;
-
             }
         }
         return overlap;
     }
+
 
 }
